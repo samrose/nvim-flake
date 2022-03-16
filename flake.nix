c164022883f94e489f01e0bf1619fc1dd450faf5
samrose <samuel.rose@gmail.com>
2022-03-16 15:50:02 -0400

    nvim configuration

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    neovim-with-config = pkgs.neovim.override {
+      configure = {
+        customRC = ''
+          set title
+          set nu
+          autocmd BufWritePre * :%s/\s\+$//e
+        '';
+        packages.package.start = with pkgs.vimPlugins; [
+          vim-nix
+          vim-parinfer
+          YouCompleteMe
+        ];
+      };
+
+      viAlias = true;
+      withPython3 = true;
+      withRuby = false;
+    };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [neovim-with-config pkgs.alejandra];
+      shellHook = ''
+        export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+      '';
+    };
+  };
+}
