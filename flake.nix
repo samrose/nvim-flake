73a2f5001dd6199b227ebc30dcf9aa3e635dfd8e
samrose <samuel.rose@gmail.com>
2022-03-16 16:34:33 -0400

    jq

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     };
   in {
     devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = [neovim-with-config pkgs.alejandra];
+      buildInputs = [neovim-with-config pkgs.alejandra pkgs.jq];
       shellHook = ''
         export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
       '';
